0f03e8670c5c6479f44ddeb065252ffc7a8148db
1198228864 <1198228864@qq.com>
2021-12-09 11:37:53 +0800

    删除字体

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store';
 import  'lib-flexible/flexible.js';
 import '../src/assets/css/iconfont.css';
 import '../src/assets/css/iconfonts.css';
-import '../src/assets/css/font.css';
+
 import '../src/assets/css/base.css';
 import common from '../src/assets/js/common';
 import project from '../src/assets/js/project';
